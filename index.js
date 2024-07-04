5c9ba8afcd6cfe52826b6cf731d9bf0f2c271277
Ria <johnria6@gmail.com>
2024-07-04 14:22:32 +0300

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+
+const prompt = require('prompt-sync')();
 function studentGradeGenerator(){
 let scores = prompt("Enter students scores (between 0 and 100):")
-if (isNaN(scores) || scores > 0 || scores < 100){
-    alert("Invalid input. Please enter a number between 0 and 100.");
+if (isNaN(scores) || scores < 0 || scores > 100){
+    console.log("Invalid input. Please enter a number between 0 and 100.");
     return;
 }
-}
-let scores = x
+
 if (scores <= 100 && scores >= 79) {
     console.log('A');
 } else if (scores <=78 && scores >=60) {
@@ -20,3 +21,5 @@ if (scores <= 100 && scores >= 79) {
 else{
     console.log('ERROR no match found')
 } 
+}
+studentGradeGenerator();
